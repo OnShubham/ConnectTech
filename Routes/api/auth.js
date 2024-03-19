c6c772492ca643a9d98636458a39546f618fbbfa
OnShubham <shubhamkharche3005@gmail.com>
2024-03-19 15:28:18 -0400

    add jtw show a data in server

--- Routes/api/auth.js
+++ Routes/api/auth.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const User = require("../../models/Users");
 
 // Protected route example
-router.get("/", auth, (req, res) => {
+router.get("/", auth, async (req, res) => {
   // Access authenticated user via req.user
-  res.send("Authenticated route, user ID: " );
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
