26dd35174fd564e405b9a52c273cb50855359797
OnShubham <shubhamkharche3005@gmail.com>
2024-03-19 13:12:41 -0400

    this project are working on a backend side

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express"); // import express
+const connectDB = require("./config/db"); // import the connectDB function from the db.js file
+
+const app = express(); // create an express server
+
+connectDB(); // connect to the database
+// deffine the routes
+app.use("/api/users", require("./Routes/api/users")); // use the users.js file for the /api/users route
+app.use("/api/auth", require("./Routes/api/auth")); // use the auth.js file for the /api/auth route
+app.use("/api/profile", require("./Routes/api/profile")); // use the profile.js file for the /api/profile route
+app.use("/api/post", require("./Routes/api/post")); // use the post.js file for the /api/post route
+
+app.get("/", (req, res) => res.send("API Runing!")); // get request to the root of the server
+
+const port = process.env.PORT || 5000; // set the port to the environment port or 5000
+
+app.listen(port, () => console.log(`Server running on port ${port}`)); // listen on the port
