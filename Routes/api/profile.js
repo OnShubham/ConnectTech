d83421aa14f6028c802d0cc3b232fc34fc15ee34
OnShubham <shubhamkharche3005@gmail.com>
2024-03-20 02:54:20 -0400

    logged api are working

--- Routes/api/profile.js
+++ Routes/api/profile.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
+
 
 // @route   GET api/Profile
 // @desc    Test route
 // @access  Public
 
-router.get('/', (req, res) => { res.send('Profile route') } );
+router.get('/me',auth, async (req, res) => { 
+
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+        if(!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user'});
+        }
+        res.json(profile);
+
+
+        
+    } catch (error) {
+
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+ } );
 
 module.exports = router;
