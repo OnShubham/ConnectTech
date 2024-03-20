b04859c2e8ff29cee320e229426c6834098179ee
OnShubham <shubhamkharche3005@gmail.com>
2024-03-20 14:27:28 -0400

    done a api are working

--- Routes/api/post.js
+++ Routes/api/post.js
@@ -1,12 +1,82 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const Post = require("../../models/Post");
+const User = require("../../models/Users");
+const Profile = require("../../models/Profile");
+
+// @route   POST api/Post
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // 400 Bad Request
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+      // 201 Created
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      // 500 Internal Server Error
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // @route   GET api/Post
-// @desc    Test route
-// @access  Public
+// @desc    Get all posts
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    // 500 Internal Server Error
+    res.status(500).send("Server Error");
+  }
+});
 
-router.get("/", (req, res) => {
-  res.send("Post route");
+// @route   DELETE api/Post/:id
+// @desc    Delete a post by ID
+// @access  Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      // 404 Not Found
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    await post.remove();
+
+    // Return a success message
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      // 404 Not Found
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    // 500 Internal Server Error
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
--- Routes/api/profile.js
+++ Routes/api/profile.js
@@ -216,4 +216,69 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
   }
 });
 
+// @route   Put api/Profile/exprience
+// @desc  Add Profile Exprience
+// @access  private
+
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      (check("school", "school is requird").not().isEmpty(),
+      check("degree", "degree is requird").not().isEmpty(),
+      check("fieldofstudy", "fieldofstudy is requird").not().isEmpty(),
+      check("from", "from is requird").not().isEmpty()),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { school, degree, fieldofstudy, from, to, current, description } =
+      req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route   Deleted api/Profile/exprience
+// @desc  Deleted Profile Exprience
+// @access  private
+
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // Get remove index
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
